8f926ece8de2552fd9ceff3cc1f698e905be9a78
Joe Forsythe <j.forsythe@modcloth.com>
2014-11-19 23:15:17 -0500

    Write out results to file
    Getting CSV writer functional.  Ultimately will be fault tolerant and more flexible

--- src/main/scala/CashRegister.scala
+++ src/main/scala/CashRegister.scala
@@ -4,12 +4,19 @@ import java.io.{File}
 object CashRegister {
   def main(args: Array[String]) {
     println(valuesFor("foo.csv"))
+    writeOut("bar.csv", List(List("a", "b"), List("baz", "c")))
   }
 
   def valuesFor(fileName: String): List[List[Double]] = {
     val reader = CSVReader.open(new File(fileName))
-    val content = reader.all().map(x => x.map(_.toDouble))
-    reader.close()
+    val content = reader.all.map(x => x.map(_.toDouble))
+    reader.close
     content
+  }
+
+  def writeOut(fileName: String, output: List[List[String]]) {
+    val writer = CSVWriter.open(new File(fileName))
+    writer.writeAll(output)
+    writer.close
   }
 }
