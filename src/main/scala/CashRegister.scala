dc745161544c04936f34e3ad3aae89bfb5a72e86
Joe Forsythe <j.forsythe@modcloth.com>
2014-11-21 14:58:01 -0500

    Implement Random
    Implemented recursive function with list of currency randomized.  I'm not sure if I'm checking for 'divisible by 3' correctly though so I need to look into that further.  Moved the USA denominations into the Currency space, seems like it belongs there instead of the part responsible for calculations

--- src/main/scala/CashRegister.scala
+++ src/main/scala/CashRegister.scala
@@ -1,9 +1,22 @@
 import com.github.tototoshi.csv._
 import java.io.{File}
+import util.{Random}
 
 object CashRegister {
 
   object Currency {
+    val USA = Map(
+      100.0 -> "100 dollar bill/100 dollar bills",
+      50.0  -> "50 dollar bill/50 dollar bills",
+      20.0  -> "20 dollar bill/20 dollar bills",
+      10.0  -> "10 dollar bill/10 dollar bills",
+      5.0  -> "5 dollar bill/5 dollar bills",
+      1.0  -> "1 dollar bill/1 dollar bills",
+      0.25  -> "quarter/quarters",
+      0.10  -> "dime/dimes",
+      0.05  -> "nickel/nickels",
+      0.01  -> "penny/pennies")
+
     def round(m: Double): Double = {
       val t = math.pow(10, 2)
       (math.round(m * t)) / t
@@ -13,7 +26,6 @@ object CashRegister {
   object Presenter {
     def format(input: List[Double], currency: Map[Double, String]): List[String] = {
       val grouped = input.groupBy(identity).values
-      println(grouped)
       grouped.toList.map(x =>
           if (x.length > 1) { "%d %s".format(x.length, currency(x.head).split("/").last) }
           else { "%d %s".format(x.length, currency(x.head).split("/").head) }
@@ -21,19 +33,9 @@ object CashRegister {
     }
   }
 
-  val USCurrency = Map(100.0 -> "100 dollar bill/100 dollar bills",
-                       50.0  -> "50 dollar bill/50 dollar bills",
-                       20.0  -> "20 dollar bill/20 dollar bills",
-                       10.0  -> "10 dollar bill/10 dollar bills",
-                        5.0  -> "5 dollar bill/5 dollar bills",
-                        1.0  -> "1 dollar bill/1 dollar bills",
-                       0.25  -> "quarter/quarters",
-                       0.10  -> "dime/dimes",
-                       0.05  -> "nickel/nickels",
-                       0.01  -> "penny/pennies")
 
   def main(args: Array[String]) {
-    writeOut("bar.csv", calculateChange(valuesFor("foo.csv"), USCurrency))
+    writeOut("bar.csv", calculateChange(valuesFor("foo.csv"), Currency.USA))
   }
 
   def valuesFor(fileName: String): List[List[Double]] = {
@@ -52,8 +54,20 @@ object CashRegister {
       else { recurs(amtLeft, curr.drop(1), res) }
     }
 
+    def recursRandom(amtLeft: Double, curr: List[Double], res: List[Double]): List[Double] = {
+      if (amtLeft <= 0.0) { return res }
+      else if (Currency.round(amtLeft - curr.head) >= 0.0) {
+        recurs(Currency.round(amtLeft - curr.head), Random.shuffle(curr), res ::: List(curr.head))
+      }
+      else { recurs(amtLeft, Random.shuffle(curr), res) }
+    }
+
     values.map(x =>
-      Presenter.format(recurs(Currency.round(x.last - x.head), currency.keys.toList.sortBy(- _.toDouble), List()), currency)
+      if ((x.head * 100) % 3 == 0) {
+        Presenter.format(recursRandom(Currency.round(x.last - x.head), currency.keys.toList, List()), currency)
+      } else {
+        Presenter.format(recurs(Currency.round(x.last - x.head), currency.keys.toList.sortBy(- _.toDouble), List()), currency)
+      }
     )
   }
 
