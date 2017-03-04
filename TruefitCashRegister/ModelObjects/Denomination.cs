144c6b39b70c01fa86274fc6b43a6e04aaa576eb
Steve <steven.faurie@gmail.com>
2017-03-04 16:03:48 -0500

    Working Change Calculator.  Uses Winforms.  Created and Tested in Visual Studio 2015 Community Edition

---
+++ TruefitCashRegister/ModelObjects/Denomination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelObjects
+{
+    public class Denomination
+    {
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+    }
+}
