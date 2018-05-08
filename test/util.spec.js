0d71e6a76fc8de969c9236e2a07be352dc0edaff
JMoli <justin@omnomstudio.io>
2018-05-08 16:28:32 -0400

    added more verbose tests, additional assertions

--- test/util.spec.js
+++ test/util.spec.js
@@ -28,29 +28,61 @@ describe('Util', function(){
 	});
 
 	describe('#calcChange.standard', function(){
-		it('should work 3000', function(){
+		it('$30 should return 1 twenty and 1 ten', function(){
 			utils.calcChange.standard(3000, 'USD', function(r){
-				console.log(r);	
+				assert.equal(r.pennies, undefined);
+				assert.equal(r.nickels, undefined);
+				assert.equal(r.dimes, undefined);
+				assert.equal(r.quarters, undefined);
+				assert.equal(r.ones, undefined);
+				assert.equal(r.fives, undefined);
+				assert.equal(r.tens, 1);
+				assert.equal(r.twenties, 1);
+				assert.equal(r.fifties, undefined);
+				assert.equal(r.hundreds, undefined);
+
 			});
 		});
-		it('should work 33233', function(){
+		it('$332.33 should return 3 hundreds, 1 twenty, 1 ten,  2 dollars, 1 quarter, 1 nickel, and 3 pennies', function(){
 			utils.calcChange.standard(33233, 'USD', function(r){
-				console.log(r);	
+				assert.equal(r.pennies, 3);
+				assert.equal(r.nickels, 1);
+				assert.equal(r.dimes, undefined);
+				assert.equal(r.quarters, 1);
+				assert.equal(r.ones, 2);
+				assert.equal(r.fives, undefined);
+				assert.equal(r.tens, 1);
+				assert.equal(r.twenties, 1);
+				assert.equal(r.fifties, undefined);
+				assert.equal(r.hundreds, 3);
 			});
 		});
-		it('should work 10', function(){
+		it('$0.10 should return 1 dime', function(){
 			utils.calcChange.standard(10, 'USD', function(r){
-				console.log(r);	
+			assert.equal(r.pennies, undefined);
+				assert.equal(r.nickels, undefined);
+				assert.equal(r.dimes, 1);
+				assert.equal(r.quarters, undefined);
+				assert.equal(r.ones, undefined);
+				assert.equal(r.fives, undefined);
+				assert.equal(r.tens, undefined);
+				assert.equal(r.twenties, undefined);
+				assert.equal(r.fifties, undefined);
+				assert.equal(r.hundreds, undefined);
 			});
 		});
-		it('should work 2305', function(){
+		it('$23.05 should return 1 twenty, 3 ones, 1 nickel', function(){
 			utils.calcChange.standard(2305, 'USD', function(r){
-				console.log(r);	
-			});
-		});
-		it('should work 30000', function(){
-			utils.calcChange.standard(30000, 'USD', function(r){
-				console.log(r);	
+				assert.equal(r.pennies, undefined);
+				assert.equal(r.nickels, 1);
+				assert.equal(r.dimes, undefined);
+				assert.equal(r.quarters, undefined);
+				assert.equal(r.ones, 3);
+				assert.equal(r.fives, undefined);
+				assert.equal(r.tens, undefined);
+				assert.equal(r.twenties, 1);
+				assert.equal(r.fifties, undefined);
+				assert.equal(r.hundreds, undefined);
 			});
 		});
 	
