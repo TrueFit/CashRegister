47323b442fa42ca2f25bcb0eb59313687e1215c9
JMoli <justin@omnomstudio.io>
2018-05-08 16:06:15 -0400

    reworked modules, finished standard change due function and recursive function

--- src/Utils.js
+++ src/Utils.js
@@ -1,27 +1,57 @@
 const Locales = require('./Locales.js');
 
-module.exports = {
-   //@PARAMS owedMaximized (int) - The Amount owed converted to the smallest denomination
-	checkModulus : function(owedMaximized){
-		return (owedMaximized % 3 === 0);
-   },
-	calcChange : function(){
-	
-		return{
-			standard: function(){
-			
-			},
-			random : function(){
-			
-			}	
-		}	
-	},
-
-   //@Params owed (float)(required) - The amount owed in the original float format
-   //@Params 'locale' (required) - The desired locale to use
-	convertToSmallest : function(owed, locale){
-	   return Locales[locale].base * owed;
-	}
-
-
+//@PARAMS owedMaximized (int) - The Amount owed converted to the smallest denomination
+module.exports.checkModulus = function(owedMaximized){
+	return (owedMaximized % 3 === 0);
 };
+
+module.exports.calcChange = function(){
+	var _working = {};
+	const _recursiveSearch = function(owedMax, locale, callback){
+		for(var key in Locales[locale].denominations){
+			var obj = Locales[locale].denominations[key]; 
+			if(owedMax >= obj.value){
+				if(_working[key] === undefined){
+					_working[key] = 1;
+				}else{
+					_working[key]++;
+				}
+				var remainder = owedMax - obj.value;
+				if(remainder > 0){
+					_recursiveSearch(remainder, locale, callback);	
+					break;
+				}else{
+					callback(_working);
+					break;
+				}
+			}else{ 
+				continue;	
+			}
+		}
+	};
+
+	return{
+		//@PARAMS owedMax(int) - the maximized owed 
+		//@PARAMs 'locale' - the desired locale
+		//@PARAMS callback
+		standard: function(owedMax, locale, callback){
+			_working = {};
+			_recursiveSearch(owedMax, locale, function(result){
+				callback(result);	
+			});
+		},
+			random : function(owedMax, locale, callback){
+				//calculate random percentages of owedMax 
+				//pick a random
+
+			}	
+	}	
+}();
+
+//@PARAMS owed (float)(required) - The amount owed in the original float format
+//@PARAMS 'locale' (required) - The desired locale to use
+module.exports.convertToSmallest = function(owed, locale){
+	return parseInt(Locales[locale].base * owed);
+}
+
+
