190986f08b36fa77992817b9f8290e02af711f41
JMoli <justin@omnomstudio.io>
2018-05-09 12:04:14 -0400

    fixed comments

--- src/CashRegister.js
+++ src/CashRegister.js
@@ -1,12 +1,12 @@
 const utils = require('./Utils.js');
 
 
-//@RETURNS {object} containing the amount owed, and any applicable warnings, and the formatted output 
-//@PARAMS - price (float) - The value of the first parameter of the csv
-//@PARAMS - paid (float) - The value of the second parameter of the csv
-//@PARAMS - 'locale' - An optional locale to make for easy currency additions
+//@RETURNS 'string' - containing the well formatted change object or error output where applicable 
+//@PARAMS - price - The value of the first parameter of the csv
+//@PARAMS - paid - The value of the second parameter of the csv
+//@PARAMS - 'locale' - Locale to make for easy currency additions
 module.exports = {
-	getChange : function getChange(price, paid, locale='USD', callback){
+	getChange : function getChange(price, paid, locale, callback){
 			paid = utils.convertToSmallest(paid, locale);
 			price = utils.convertToSmallest(price, locale);
 			var owed = paid - price;
