8910d4f9d7c1fe7e2e3d521fad3137586469e20c
JMoli <justin@omnomstudio.io>
2018-05-09 11:36:38 -0400

    added callbacks, fixed wrong input number bug

--- src/CashRegister.js
+++ src/CashRegister.js
@@ -6,32 +6,35 @@ const utils = require('./Utils.js');
 //@PARAMS - paid (float) - The value of the second parameter of the csv
 //@PARAMS - 'locale' - An optional locale to make for easy currency additions
 module.exports = {
-	getChange : function getChange(price, paid, locale='USD'){
-			var owed = price - paid;
-			if( owed > 0){
-				owed = utils.convertToSmallest(owed, locale);
-				if(utils.checkModulus(owed)){
+	getChange : function getChange(price, paid, locale='USD', callback){
+			paid = utils.convertToSmallest(paid, locale);
+			price = utils.convertToSmallest(price, locale);
+			var owed = paid - price;
+
+			if(owed > 0){
+				if(utils.checkModulus(price)){
 					//is divisible by 3 return random string
-					return utils.calcChange.random(owed, locale);
+					utils.calcChange.random(owed, locale, function(data){
+						callback( utils.formatString(data) );	
+					});
 				}else{
 					//calculate 
-					return utils.calcChange.standard(owed, locale);
+					utils.calcChange.standard(owed, locale, function(data){
+						callback( utils.formatString(data) );	
+					});
 				}
-				//convert to float
-				//convert total amount to pennies
-
 			}else{
 				if(owed < 0){
 					//TODO add test case for this
 					//Full Change is due
-					return 'Invalid Transaction';
+					callback('Invalid Transaction');
 				}else{
-					if(parseInt(price) === 0 && parseInt(paid) === 0){
+					if(owed === 0){
 						//TODO add test case for this
-						return 'Invalid Transaction - Someone robbed the register';
+						callback('Invalid Transaction - Someone robbed the register');
 					}else{
 						//TODO add test case for this
-						return 'Exact Change - Nothing is Owed';	
+						callback('Exact Change - Nothing is Owed');	
 					}
 				}
 			}
