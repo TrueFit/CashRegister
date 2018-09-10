1ef6920633bf156d1e5c763b9f20181f58dca572
Jake <walcott@Walcotts-MacBook-Pro.local>
2018-09-10 19:09:37 -0400

    Clean up calculations and add parsing function

--- cash-register/src/app/calculator/calculator.component.ts
+++ cash-register/src/app/calculator/calculator.component.ts
@@ -14,7 +14,7 @@ export class CalculatorComponent implements OnInit {
   }
 
   read() {
-    let files: any;
+    let answerArray = [];
     let input = event.target;
     for (var index = 0; index < input.files.length; index++) {
         let reader = new FileReader();
@@ -25,9 +25,12 @@ export class CalculatorComponent implements OnInit {
             for (var i = 0; i < inputs.length; i++) {
               if (inputs[i].length != 1) {    // checks that the string is NOT empty
                 var splitValues = inputs[i].split(",");
-                this.calculate(splitValues);
+                var calculatedChange = this.calculate(splitValues);
+                answerArray.push(calculatedChange)
               }
             }
+            console.log(answerArray);
+            this.write(answerArray);
         }
         reader.readAsText(input.files[index]);
     };
@@ -44,28 +47,43 @@ export class CalculatorComponent implements OnInit {
       {name: "nickle", value: 0.05},
       {name: "penny", value: 0.01}
     ];
-    var change = values[1] - values[0];
-    console.log(change);
-    var result = denominations.reduce(function(acc, next) {   // iterates through the denomination object from top to bottom
-      console.log(next.name)
-      if (change >= next.value) {
+    var change = values[1] - values[0];   // get the difference between what is paid and owed
+    var result = denominations.reduce(function(accumulator, currentDenomination) {   // iterates through the denomination object from top to bottom
+      console.log(currentDenomination.name)
+      if (change >= currentDenomination.value) {
         var currentValue = 0.00;    // the amount of coins/bills for each denomination
-        while (change >= next.value && change >= 0) {
+        while (change >= currentDenomination.value && change >= 0) {
           currentValue ++;
-          change -= next.value;
+          change -= currentDenomination.value;
           change = Math.round(change * 100) / 100   // prevents nasty decimal issues in TypeScript
-          console.log(change)
         }
-        acc.push([next.name, currentValue]);
-        console.log(acc)
-        return acc
+        accumulator.push([currentDenomination.name, currentValue]);
+        return accumulator
       } else {
-        return acc
-        console.log(acc)
+        return accumulator
       }
     }, []);
+    // console.log(result)
     return result
-    console.log(result)
+  }
+
+  write(answerArray) {
+    let csvContent = "data:text/csv;charset=utf-8,";
+    // TODO: not the most graceful solution, use objects instead?
+    for (var i = 0; i < answerArray.length; i++) {    // each iteration is one transaction
+      console.log(answerArray[i])
+      for (var x = 0; x < answerArray[i].length; x++) {   // each iteration is one denomination of a transaction
+        var string = answerArray[i][x][1] + " " + answerArray[i][x][0]
+        var transactionString = transactionString + " " + string;
+        console.log(transactionString)
+      }
+    }
+    // rows.forEach(function(rowArray){
+    //   let row = rowArray.join(",");
+    //   csvContent += row + "\r\n";
+    // });
+    // var encodedUri = encodeURI(csvContent);
+    // window.open(encodedUri);
   }
 
 }
