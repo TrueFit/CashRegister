423da7ffde11d7b79fc2ee4944fdea87c07f0861
Jake <jakedenison16@gmail.com>
2018-09-12 15:51:35 -0400

    Refactor and test

--- cash-register/src/app/calculator/calculator.component.ts
+++ cash-register/src/app/calculator/calculator.component.ts
@@ -16,18 +16,27 @@ export class CalculatorComponent {
     as well as parsing and passing the raw values to the appropriate calculation
     function */
     var resultArray = [];
-    var input = event.target;    // the file from the UI element
+    var input: any = event.target;
     var reader = new FileReader();
     reader.onload = () => {
         var rawData = reader.result;
         let transactions = rawData.split("\n");   // returns an array of the owed/paid sets
         for (var i = 0; i < transactions.length; i++) {
-          if (transactions[i].length != 1) {    // checks that the string is NOT empty (parsing can create empty strings)
+          if (transactions[i].length != 1 && transactions[i] != "") {   // checks that the string is NOT empty (parsing can create empty strings)
             var splitTransactions = transactions[i].split(",");
-            var change = splitTransactions[1] - splitTransactions[0]   // TODO add checks for negative/0 stuff here
+            var change = splitTransactions[1] - splitTransactions[0];
+            if (Math.sign(change) == -1) {    // exception for insufficent funds
+              // alert("The transaction " + splitTransactions + " could not be calculated because the amount paid is insufficent.")
+              resultArray.push([{name: "insufficent funds"}]);
+              continue;
+            } else if (change == 0){    // exception for no change due
+              // alert("No change is due for the transaction " + splitTransactions)
+              resultArray.push([{name: "no change due"}]);
+              continue;
+            }
             if ((splitTransactions[0] * 100) % 3 === 0) {   // checks if the amount owed is divisible by 3
               var randomResults = this.calculateRandom(change);    // passes values to random calculator
-              resultArray.push(randomResults)
+              resultArray.push(randomResults);
             } else {
               var standardResults = this.calculateStandard(change);    // passes values to standard calculator
               resultArray.push(standardResults);
@@ -48,7 +57,7 @@ export class CalculatorComponent {
         while (change >= currentDenomination.value) {
           currentValue ++;
           change -= currentDenomination.value;
-          change = Math.round(change * 100) / 100   // prevents nasty decimal issues in TypeScript
+          change = Math.round(change * 100) / 100;   // prevents nasty decimal issues in TypeScript
         }
         if (currentValue > 1) {   // checks to see if the plural denomination name should be used or not
           accumulator.push({name: currentDenomination.plural, amount: currentValue});
@@ -60,7 +69,7 @@ export class CalculatorComponent {
         return accumulator;
       }
     }, []);   // the empty array is the initial accumulator
-    return result
+    return result;
   }
 
 
@@ -68,38 +77,37 @@ export class CalculatorComponent {
     /* this function calculates the randomized denominations if the amount
     due on a transaction is divisible by 3. It's not the most efficent randomization
     system, but it works well */
-    let result = []
-    var totalValue = 0
+    let result = [];
+    var totalValue = 0;
     while (totalValue < change) {
       var sameDenominationStatus = false;   // resets the same denomination catch for each iteration
       var randomDenomination = this.denominations[Math.floor(Math.random() * this.denominations.length)];   // selects a random denomination
       if (change >= randomDenomination.value) {   // makes sure the denomination is valid
-        var denominationAmount = Math.floor((Math.random() * 10) + 1) // picks a random amount of the random denomination
+        var denominationAmount = Math.floor((Math.random() * 10) + 1); // picks a random amount of the random denomination
         if (change >= (denominationAmount * randomDenomination.value) + totalValue && denominationAmount != 0) {    // makes sure the values are still valid
-          totalValue += denominationAmount * randomDenomination.value
-          totalValue = Math.round(totalValue * 100) / 100   // prevents nasty decimal issues
+          totalValue += denominationAmount * randomDenomination.value;
+          totalValue = Math.round(totalValue * 100) / 100;   // prevents nasty decimal issues
           for (var i = 0; i < result.length; i++) {   // combines duplicate denominations
             if (result[i].name == randomDenomination.name || result[i].name == randomDenomination.plural) {
               result[i].amount += denominationAmount;
-              if (result[i].amount > 1) {
-                result[i].name = randomDenomination.plural
+              if (result[i].amount > 1) {   // checks to see if the denomination needs to switch to plural
+                result[i].name = randomDenomination.plural;
               }
               var sameDenominationStatus = true;
             }
           }
           if (sameDenominationStatus != true) {   // if the denomination is not a duplicate, we add it to the array
             if (denominationAmount > 1) {   // checks to use singular or plural name
-              result.push({name: randomDenomination.plural, amount: denominationAmount, value: randomDenomination.value})
+              result.push({name: randomDenomination.plural, amount: denominationAmount, value: randomDenomination.value});
             } else {
-              result.push({name: randomDenomination.name, amount: denominationAmount, value: randomDenomination.value})
+              result.push({name: randomDenomination.name, amount: denominationAmount, value: randomDenomination.value});
             }
           }
         }
       }
     }
-    result = result.sort(function(a, b) {return b.value - a.value});
-    console.log(result)
-    return result
+    result = result.sort(function(a, b) {return b.value - a.value});    // sort the denominations in descending order
+    return result;
   }
 
 
@@ -109,20 +117,26 @@ export class CalculatorComponent {
     var options = {   // options for CSV export package
       headers: ["Transaction Number", "Change Due"]
     }
-    let stringAnswers = []    // init our array of final, human readable answers
-    let transactionCounter = 0    // matches each transaction with the appropriate answer line
+    let stringAnswers = [];    // init our array of final, human readable answers
+    let transactionCounter = 0;    // matches each transaction with the appropriate answer line
     let csvContent = "data:text/csv;charset=utf-8,";
     for (var i = 0; i < resultArray.length; i++) {    // each iteration is one transaction
-      transactionCounter ++
-      let transactionString = ""
+      transactionCounter ++;
+      let transactionString = "";
       for (var x = 0; x < resultArray[i].length; x++) {   // each iteration is one denomination of a transaction
-        var string = resultArray[i][x].amount + " " + resultArray[i][x].name
+        if (resultArray[i][x].name == "insufficent funds" || resultArray[i][x].name == "no change due") {
+          var string: any = resultArray[i][x].name;
+        } else {
+          var string: any = resultArray[i][x].amount + " " + resultArray[i][x].name;
+        }
         transactionString = transactionString + string + ",";
       }
       stringAnswers.push({transaction: transactionCounter, string: transactionString});
-      console.log(stringAnswers)
+      var fileInputField = document.getElementById("file_input") as HTMLInputElement;   // black magic to avoid TypeScript quirk
+      fileInputField.value = "";
     }
-    // new Angular5Csv(stringAnswers, 'Results', options);   // third party package to generate the output CSV
+    new Angular5Csv(stringAnswers, 'Results', options);   // third party package to generate the output CSV
+    alert("One or more of your transactions were invalid. Check the results file for details.")
   }
 
 }
