137b080064d64e166d735b9190385b902de0a7f0
Christopher <Chris-Slade@users.noreply.github.com>
2019-02-06 14:27:15 -0500

    Initial commit for backend
    Initialize Express server for the backend. This will serve the API for
    cash-register calculations. Separating the front and backend will allow
    us to deploy and test them separately.

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+import * as express from 'express';
+
+const app = express();
+
+app.set('port', process.env.PORT || 3001);
+
+app.get('/status', (req: any, res: any) => {
+  res.status(200).send('OK');
+});
+
+export default app;
---
+++ backend/test/test.ts
@@ -0,0 +1,12 @@
+import * as assert from 'assert';
+import { describe, it } from 'mocha';
+import { test } from '../src/cashregister';
+
+// TODO
+describe('CashRegister', () => {
+  describe('test', () => {
+    it('should return "abc"', () => {
+      assert.strictEqual(test(), "abc");
+    });
+  });
+});
