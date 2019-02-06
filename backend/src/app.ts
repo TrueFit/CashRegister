24bcabb5154de7a37c48c16479f0bc938201d118
Christopher <Chris-Slade@users.noreply.github.com>
2019-02-06 16:49:37 -0500

    Fix port for backend server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 
 const app = express();
 
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3000);
 
 app.get('/status', (req: any, res: any) => {
   res.status(200).send('OK');
