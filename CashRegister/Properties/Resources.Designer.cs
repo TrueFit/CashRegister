4806e4e7a94f40a94d182febed17799ffdc0732b
Fallecker, Joe <jfallecker@indsci.com>
2019-07-22 16:00:43 -0400

    Update MainWindow with basic interface. Add "Browse..." button to locate the input file. Allow editable input field so that after loading, inputs can be updated as needed. Restrict editing of outputs.

--- CashRegister/Properties/Resources.Designer.cs
+++ CashRegister/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace CashRegister.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace CashRegister.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CashRegister.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace CashRegister.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace CashRegister.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Application Language.
         /// </summary>
-        internal static string Settings_CurrentLanguageLabel {
+        public static string Settings_CurrentLanguageLabel {
             get {
                 return ResourceManager.GetString("Settings_CurrentLanguageLabel", resourceCulture);
             }
@@ -72,7 +72,7 @@ namespace CashRegister.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Divisor for Random Change feature:.
         /// </summary>
-        internal static string Settings_RandomDivisorLabel {
+        public static string Settings_RandomDivisorLabel {
             get {
                 return ResourceManager.GetString("Settings_RandomDivisorLabel", resourceCulture);
             }
