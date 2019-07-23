da14a14f63e198dbed2a2bb76847decf2d56fb42
Joe Fallecker <jmfallecker@gmail.com>
2019-07-22 20:24:40 -0400

    Add properties to IMainViewModel.cs and implement them in MainViewModel.cs.
    Set the DataContext of MainWindow to be MainViewModel

--- CashRegister.ViewModels/Implementation/MainViewModel.cs
+++ CashRegister.ViewModels/Implementation/MainViewModel.cs
@@ -1,9 +1,32 @@
-﻿using CashRegister.ViewModels.Interfaces;
+﻿using System.IO;
+using System.Text;
+using System.Windows.Input;
+using CashRegister.ViewModels.Interfaces;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace CashRegister.ViewModels.Implementation
 {
     public class MainViewModel : ViewModelBase, IMainViewModel
     {
+        private string _inputPath;
+        public string InputFilePath
+        {
+            get => _inputPath;
+            set => Set(ref _inputPath, value);
+        }
+
+        public ICommand LoadFileCommand
+        {
+            get { return new RelayCommand(() => ReadFile(InputFilePath)); }
+        }
+
+        private StringBuilder _inputTextBuilder = new StringBuilder();
+        public string InputText => _inputTextBuilder.ToString();
+
+        private void ReadFile(string inputFilePath)
+        {
+            // TODO: implement and set the InputText via _inputTextBuilder       
+        }
     }
 }
--- CashRegister.ViewModels/Interfaces/IMainViewModel.cs
+++ CashRegister.ViewModels/Interfaces/IMainViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace CashRegister.ViewModels.Interfaces
+﻿using System.IO;
+using System.Windows.Input;
+
+namespace CashRegister.ViewModels.Interfaces
 {
     public interface IMainViewModel
     {
+        string InputFilePath { get; set; }
+        ICommand LoadFileCommand { get; }
+        string InputText { get; }
     }
 }
