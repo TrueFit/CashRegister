5ef4867c60eee4b6efb754c4b13924ad2a8a1244
Kim Cooperrider <kim@retirementatlas.com>
2020-01-03 12:13:52 -0500

    min denoms

---
+++ src/constants/currency.js
@@ -0,0 +1,53 @@
+export const Currency = {
+    USD: 'usd',
+}
+
+export const USD = {
+    id: Currency.USD,
+    denominations: [
+        { value: 1, singular: 'penny', plural: 'pennies' },
+        { value: 5, singular: 'nickel', plural: 'nickels' },
+        { value: 10, singular: 'dime', plural: 'dimes' },
+        {
+            value: 25,
+            singular: 'quarter',
+            plural: 'quarters',
+        },
+        {
+            value: 100,
+            singular: 'dollar',
+            plural: 'dollars',
+        },
+        {
+            value: 500,
+            singular: '5 dollar bill',
+            plural: '5 dollar bills',
+        },
+        {
+            value: 1000,
+            singular: '10 dollar bill',
+            plural: '10 dollar bills',
+        },
+        {
+            value: 2000,
+            singular: '20 dollar bill',
+            plural: '20 dollar bills',
+        },
+        {
+            value: 5000,
+            singular: '50 dollar bill',
+            plural: '50 dollar bills',
+        },
+        {
+            value: 10000,
+            singular: '100 dollar bill',
+            plural: '100 dollar bills',
+        },
+    ],
+    symbol: '$',
+    formatAmount: amount => `$${amount}`,
+    normalizeAmount: amount => 100 * amount,
+    denormalizeAmount: amount => amount / 100,
+}
+
+export const Currencies = [USD]
--- src/main.js
+++ src/main.js
@@ -17,39 +17,7 @@ import { promises as fs } from 'fs'
 import { argv } from 'yargs'
 import path from 'path'
 import { Logger } from 'utils'
-
-const ArraySort = {
-    ASC: 'ascending',
-    RANDOM: 'random',
-}
-
-const Currency = {
-    USD: 'usd',
-}
-
-const USD = {
-    id: Currency.USD,
-    denominations: [
-        { value: 1, singular: 'penny', plural: 'pennies' },
-        { value: 5, singular: 'nickel', plural: 'nickels' },
-        { value: 10, singular: 'dime', plural: 'dimes' },
-        {
-            value: 25,
-            singular: 'quarter',
-            plural: 'quarters',
-        },
-    ],
-    symbol: '$',
-    formatAmount: amount => `$${amount}`,
-    normalizeAmount: amount => 100 * amount,
-    denormalizeAmount: amount => amount / 100,
-}
-
-const Currencies = [USD]
-
-const sortDenominationsAsc = array => [
-    ...array.sort((a, b) => a.value - b.value),
-]
+import { Currency, Currencies } from 'constants/currency'
 
 // Durstenfled shuffle algo
 function sortArrayRandom(array) {
@@ -63,23 +31,82 @@ function sortArrayRandom(array) {
     return shuffled
 }
 
-const sortFunctionMap = {
-    [ArraySort.ASC]: sortDenominationsAsc,
-    [ArraySort.RANDOM]: sortArrayRandom,
+const hasValues = entry => Array.isArray(entry)
+
+// Append the current denomination to the previous list of denominations IF:
+// A) `denomsForAmount[subAmount]` has no values,
+// meaning we don't yet have any combos of denominations that will sum to `subAmount`.
+// -- OR --
+// B) The length of `prevDenoms` plus 1 for the current denom is less than
+// the length of the previous entry for this `subAmount`, ie it takes fewer denoms to make `subAmount`.
+const shouldAppendCurrentDenom = ({ prevDenoms, subAmount, denomsForAmount }) =>
+    !hasValues(denomsForAmount[subAmount]) ||
+    prevDenoms.length + 1 < denomsForAmount[subAmount].length
+
+const getMinDenoms = (denominations, amount) => {
+    // Init 2D array that will store the optimal solution for each value (`subAmount`) from 0 to `amount`.
+    // Each entry is the array of the minimum denominations needed to compose `subAmount`.
+    const denomsForAmount = new Array(amount + 1)
+
+    // Base case: 0 coins are needed to make amount of 0
+    denomsForAmount[0] = []
+
+    // Now, solve for all amounts 1-`amount`
+    for (let subAmount = 1; subAmount <= amount; subAmount++) {
+        // Init each to null
+        denomsForAmount[subAmount] = null
+
+        // Try every denomination one a time to find the minimum combo
+        // for each `subAmount`
+        for (let i = 0; i < denominations.length; i++) {
+            const denom = denominations[i]
+            const { value } = denom
+
+            if (value <= subAmount) {
+                const prevDenoms = denomsForAmount[subAmount - value]
+                if (
+                    shouldAppendCurrentDenom({
+                        subAmount,
+                        denomsForAmount,
+                        prevDenoms,
+                    })
+                ) {
+                    denomsForAmount[subAmount] = [...prevDenoms, denom]
+                }
+            }
+        }
+    }
+
+    // Return optimal solution for given `amount`
+    return denomsForAmount[amount]
 }
 
-const getChange = ({ owed, currency, sort = ArraySort.ASC }) => {
+const formatMinDenoms = entries => {
+    // `entries` is an array of denominations.
+    // Here we format the array into a shape mapping `value` for each entry to { count, denomination }
+
+    return entries.reduce((acc, denomination) => {
+        const { value } = denomination
+
+        return {
+            ...acc,
+            [value]: {
+                denomination,
+                count: (acc[value]?.count || 0) + 1,
+            },
+        }
+    }, {})
+}
+
+const getRandomDenoms = ({ owed, currency }) => {
     // Initially, the amount equals just the normalized `owed`
     let amount = currency.normalizeAmount(owed)
 
-    // `sort` just to be sure we have them in ASC order
-    const denominations = sortFunctionMap[sort].call(
-        null,
-        currency.denominations
-    )
+    // `sort` to randomly arrange the denominations
+    const denominations = currency.denominations.sort(sortArrayRandom)
 
-    // `result` will be an array of arrays in the form [piece, count]
-    const result = []
+    // `result` will be an object mapping the `value` of the denomination to { count, denomination }
+    let result = {}
 
     while (amount > 0) {
         // Get the next greatest value
@@ -97,7 +124,13 @@ const getChange = ({ owed, currency, sort = ArraySort.ASC }) => {
         // Reduce `amount`
         amount -= count * value
 
-        result.push({ count, denomination })
+        result = {
+            ...result,
+            [denomination.value]: {
+                count,
+                denomination,
+            },
+        }
     }
 
     return result
@@ -139,6 +172,7 @@ const main = async ({ currencyId, divisor, inputFile, outputFile }) => {
         ({ id }) => id.toLowerCase() === currencyId.toLowerCase()
     )
     if (!currency) {
+        Logger.error('Currency not recognized')
         return null
     }
 
@@ -148,30 +182,38 @@ const main = async ({ currencyId, divisor, inputFile, outputFile }) => {
     for (const entry of entries) {
         const [owed, paid] = entry.split(',')
 
-        let params
+        let results
         let subject
 
         if (shouldRandomize({ owed, currency, divisor })) {
-            params = { owed, currency, sort: ArraySort.RANDOM }
             subject = `Can randomly make ${currency.formatAmount(
                 owed
             )} with the following: %s`
+
+            results = getRandomDenoms({
+                owed,
+                currency,
+            })
         } else {
-            params = { owed, currency, sort: ArraySort.ASC }
+            results = formatMinDenoms(
+                getMinDenoms(
+                    currency.denominations,
+                    currency.normalizeAmount(owed)
+                )
+            )
+
             subject = `Can make ${currency.formatAmount(
                 owed
             )} with the following: %s`
         }
 
-        const change = getChange(params)
-        const contents = change
-            .map(({ count, denomination }) =>
-                prettifyEntry({ count, denomination })
-            )
-            .join(', ')
-        output.push(contents)
+        const entryOutput = Object.keys(results).reduce((acc, key) => {
+            const { count, denomination } = results[key]
+            return [...acc, prettifyEntry({ count, denomination })]
+        }, [])
 
-        Logger.info(subject, contents)
+        Logger.info(subject, entryOutput.join(', '))
+        output.push(entryOutput)
     }
 
     try {
@@ -185,6 +227,7 @@ main({
     currencyId: argv.currency || Currency.USD,
     divisor: argv.divisor || 3,
     outputFile: argv.outputFile || path.join(__dirname, 'out.txt'),
+
     // Note that `inputFile` is required
     inputFile: argv.inputFile,
 })
